62abaeb5fc592a355dfc3c0a10b189d61b7c531e
arthurfsantana-code <arthur.f.santana@edu.senai.br>
2026-08-12 11:17:32 -0300

    Delete concessionaria-api/concessionaria-api directory

--- concessionaria-api/concessionaria-api/src/main/java/com/marcelogomes/concessionaria/model/StatusCarro.java
+++
@@ -1,8 +0,0 @@
-package com.marcelogomes.concessionaria.model;
-
-// disponivel = pode negociar, reservado = cliente ja deu sinal, vendido = fechou negocio
-public enum StatusCarro {
-    DISPONIVEL,
-    RESERVADO,
-    VENDIDO
-}
--- concessionaria-api/concessionaria-api/src/main/java/com/marcelogomes/concessionaria/repository/CarroRepository.java
+++
@@ -1,8 +0,0 @@
-package com.marcelogomes.concessionaria.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.marcelogomes.concessionaria.model.Carro;
-
-public interface CarroRepository extends JpaRepository<Carro, Long> {
-}
